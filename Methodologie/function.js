27326008de15c762c38aa2f017444a378803fc98
Katian <katian.kibio@outlook.com>
2022-01-12 01:08:58 +0100

    creation du projet revisions JS

---
+++ Methodologie/function.js
@@ -0,0 +1,17 @@
+/**
+ * Objet dans lequel on regroupe nos fonctions
+ */
+
+//Objet contenant les fonctions listener
+var listenerFunction = {
+	h2DblClick: ()=>{
+		window.alert("click détecté sur la balise H2")
+		console.log("click détecté sur la balise H2")
+	}
+}
+//Mise en place des abonnementq
+var	setupListeners = ()=>{
+	var h2 = document.querySelector('section#html h2')
+		h2.addEventListener('dblclick', listenerFunction.h2DblClick)
+	}
+}
